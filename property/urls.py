fc965f84ba313d93fbec15ae3183c6f568ce7d89
Mohit Nagpal <nagpalm7@gmail.com>
2019-10-12 20:57:10 +0530

    rest oauth url setup

--- property/urls.py
+++ property/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
-urlpatterns = []
-#     path('admin/', admin.site.urls),
-#     path('', include('api.urls')),
-# ]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
+]
+
+urlpatterns += [
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
